357d29beb84b7c36ff10e78bd0e7cf35f9a53de3
alicjadabr <alicja.dab27@gmail.com>
2021-02-22 17:51:59 +0100

    lista 1

---
+++ cw_1/1_2_5/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double x;
+    scanf("%lf", &x);
+    printf("%f", x);
+    return 0;
+}
---
+++ cw_1/1_2_8/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,y,z;
+    scanf("%d", &x);
+    scanf("%d", &y);
+    scanf("%d", &z);
+    printf("%f", (x+y+z)/3.0);
+    return 0;
+}
